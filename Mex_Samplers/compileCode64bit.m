71cd13ea4f3882d91a36c77e460aacc40ca07001
timothyrubin <timrubin@indiana.edu>
2016-07-07 22:07:11 -0400

    commit2

--- Mex_Samplers/compileCode64bit.m
+++ Mex_Samplers/compileCode64bit.m
@@ -2,9 +2,8 @@
 
 mex GibbsSampler_TagLDA_DCAlpha_V04.c -largeArrayDims
 
-%% This one gives error
+%% This one gives error on some machines but uses a work-around in code to handle errors
 mex GibbsSamplerLDA_v2.c -largeArrayDims
-%%
-mex GibbsSamplerLDATAGS2.c -largeArrayDims
+
 %%
 mex GibbsSamplerLDATAGS3.c -largeArrayDims
